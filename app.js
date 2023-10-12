38461f2f901dcc4016b99b4e731470645630e53e
Djordje Mihajlovic <djdjdjordje@gmail.com>
2023-10-12 20:45:20 +0100

    updates!

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ function alertButton() {
     alert("hello world!");
 }
 
-document.getElementById('pdfButton').addEventListener('click', function() {
-    // Replace 'path-to-your-file.pdf' with the actual path to your PDF file
-    var pdfPath = 'pdf/CV-MIHAJLOVIC Djordje-2 copy.pdf';
+// document.getElementById('pdfButton').addEventListener('click', function() {
+//     // Replace 'path-to-your-file.pdf' with the actual path to your PDF file
+//     var pdfPath = 'pdf/CV-MIHAJLOVIC Djordje-2 copy.pdf';
 
-    // Open the PDF file in a new tab or window
-    window.open(pdfPath, '_blank');
-});
+//     // Open the PDF file in a new tab or window
+//     window.open(pdfPath, '_blank');
+// });
+
+function scrollToSection(sectionId) {
+    // Get the target element by its ID
+    var targetElement = document.getElementById(sectionId);
+
+    // Scroll to the target element
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+}
