b568309a9dfa8476f92a8f57e2afba831e26f20f
Djordje Mihajlovic <djdjdjordje@gmail.com>
2023-10-13 13:57:15 +0100

    buttons right side

--- app.js
+++ app.js
@@ -19,3 +19,50 @@ function scrollToSection(sectionId) {
         targetElement.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+var scrollButton = document.getElementById("scrollButton");
+
+  // Get the target div element
+  var targetDiv = document.getElementById("content"); // Change "content" to the ID of your specific div
+
+  // Function to check scroll position and toggle button visibility
+//   function checkScroll() {
+//     if (window.scrollY > targetDiv.offsetHeight) {
+//       scrollButton.style.display = "block";
+//     } else {
+//       scrollButton.style.display = "none";
+//     }
+//   }
+
+  // Function to scroll to the top
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+  // Attach the checkScroll function to the scroll event
+
+  window.addEventListener('scroll', function () {
+    var scrollButton = document.getElementById('scrollButton');
+    var scrollButton2 = document.getElementById('scrollButton2');
+    var scrollButton3 = document.getElementById('scrollButton3');
+    var scrollButton4 = document.getElementById('scrollButton4');
+    var scrollButton5 = document.getElementById('scrollButton5');
+
+    // Adjust the value (e.g., 200) based on when you want the buttons to appear
+    if (window.scrollY > targetDiv.offsetHeight) {
+        scrollButton.style.display = 'block';
+        scrollButton2.style.display = 'block';
+        scrollButton3.style.display = 'block';
+        scrollButton4.style.display = 'block';
+        scrollButton5.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+        scrollButton2.style.display = 'none';
+        scrollButton3.style.display = 'none';
+        scrollButton4.style.display = 'none';
+        scrollButton5.style.display = 'none';
+    }
+});
